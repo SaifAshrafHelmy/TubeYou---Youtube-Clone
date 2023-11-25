378e5e27b62f4d3f337f58b2c229c982004d0d47
saifashrafhelmy <saifashrafhelmy@yahoo.com>
2023-11-25 20:04:11 +0200

    Improved Sidebar responsiveness

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -11,25 +11,14 @@ import {
 import logo from '../assets/yt-logo.png';
 import Button from '../components/Button';
 import { useState } from 'react';
+import { useSidebarContext } from '../context/SidebarContext';
 
 const PageHeader = () => {
   const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
 
   return (
     <div className="flex justify-between gap-10 lg:gap-20 pt-2 mb-6 mx-4">
-      <div
-        className={`items-center gap-4 flex-shrink-0 ${
-          showFullWidthSearch ? 'hidden md:flex' : 'flex'
-        }`}
-      >
-        <Button variant={'ghost'} size={'icon'}>
-          <Menu />
-        </Button>
-        <a href="#">
-          <img src={logo} alt="TubeYou Logo" className="h-6" />
-        </a>
-      </div>
-
+      <PageHeaderFirstSection hidden={showFullWidthSearch} />
       <form
         className={`gap-4 flex-grow justify-center ${
           showFullWidthSearch ? 'flex' : 'hidden md:flex'
@@ -97,4 +86,28 @@ const PageHeader = () => {
   );
 };
 
+type PageHeaderFirstSectionProps = {
+  hidden?: boolean;
+};
+
+export function PageHeaderFirstSection({
+  hidden = false,
+}: PageHeaderFirstSectionProps) {
+  const { toggle } = useSidebarContext();
+
+  return (
+    <div
+      className={`items-center gap-4 flex-shrink-0 ${
+        hidden ? 'hidden md:flex' : 'flex'
+      }`}
+    >
+      <Button onClick={toggle} variant={'ghost'} size={'icon'}>
+        <Menu />
+      </Button>
+      <a href="#">
+        <img src={logo} alt="TubeYou Logo" className="h-6" />
+      </a>
+    </div>
+  );
+}
 export default PageHeader;
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -19,11 +19,18 @@ import { Children, ElementType, ReactNode, useState } from 'react';
 import Button, { buttonStyles } from '../components/Button';
 import { twMerge } from 'tailwind-merge';
 import { playlists, subscriptions } from '../data/sidebar';
+import { useSidebarContext } from '../context/SidebarContext';
+import { PageHeaderFirstSection } from './PageHeader';
 
 const Sidebar = () => {
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext();
   return (
     <>
-      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <aside
+        className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 ${
+          isLargeOpen ? 'lg:hidden' : 'lg:flex'
+        }`}
+      >
         <SmallSidebarItem IconOrImgUrl={HomeIcon} title="Home" url="/" />
         <SmallSidebarItem IconOrImgUrl={Repeat} title="Shorts" url="/shorts" />
         <SmallSidebarItem
@@ -37,7 +44,21 @@ const Sidebar = () => {
           url="/library"
         />
       </aside>
-      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 hidden lg:flex">
+      {isSmallOpen && (
+        <div
+          onClick={close}
+          className="lg:hidden fixed inset-0 z-[99] bg-secondary-dark opacity-50"
+        />
+      )}
+      <aside
+        className={`w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2
+        ${isLargeOpen ? 'lg:flex' : 'lg:hidden'} ${
+          isSmallOpen ? 'flex z-[99] bg-white max-h-screen' : 'hidden'
+        }  `}
+      >
+        <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
+          <PageHeaderFirstSection />
+        </div>
         <LargeSidebarSection>
           <LargeSidebarItem
             isActive
@@ -200,7 +221,7 @@ function LargeSidebarItem({
       className={twMerge(
         buttonStyles({ variant: 'ghost' }),
         `w-full flex items-center rounded-lg gap-4 p-3 ${
-          isActive ? 'font-bold bg-neural 100 hover:bg-secondary' : undefined
+          isActive ? 'font-bold bg-gray-100' : undefined
         }`
       )}
     >
